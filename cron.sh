0fa2112b4669e96683f5964a08f9d91110dceb6a
Larry Hamel <larhamel@gmail.com>
2025-02-06 10:59:26 -0800

    name of props

--- cron.sh
+++ cron.sh
@@ -9,4 +9,4 @@ PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
 
 export PATH=$PATH >> $LOG 2>&1
 cd "$INSTALL_DIR"
-direnv exec $INSTALL_DIR bash -c "cd $INSTALL_DIR >> $LOG 2>&1  && ./notify.py  ./opportunities.properties >> $LOG 2>&1" >> $LOG 2>&1
+direnv exec $INSTALL_DIR bash -c "cd $INSTALL_DIR >> $LOG 2>&1  && ./notify.py  ./guild.properties >> $LOG 2>&1" >> $LOG 2>&1
