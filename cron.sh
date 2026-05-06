3b51cf9d62e4ff33590d5fe9b9bab267095f6b2f
Patti Schank <codebabe@gmail.com>
2026-05-06 16:37:09 -0700

    fix command file name

--- cron.sh
+++ cron.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # cron script, expecting to be run ALREADY IN CORRECT current directory
-# run via something like: 0 9-22 * * * /usr/bin/bash $INSTALL_DIR/cron.sh >> $LOG 2>&1
+# run via something like: */30 9-22 * * * /usr/bin/bash $INSTALL_DIR/cron.sh >> $LOG 2>&1
 
 LOG=/tmp/opportunities.log
 
@@ -12,4 +12,4 @@ PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
 
 export PATH=$PATH >> $LOG 2>&1
 cd "$INSTALL_DIR"
-direnv exec $INSTALL_DIR bash -c "cd $INSTALL_DIR >> $LOG 2>&1  && ./notify.py  ./guild.properties >> $LOG 2>&1" >> $LOG 2>&1
+direnv exec $INSTALL_DIR bash -c "cd $INSTALL_DIR >> $LOG 2>&1  && ./opportunities.py  ./guild.properties >> $LOG 2>&1" >> $LOG 2>&1
